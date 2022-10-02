291affc2ed6f70fcd0a78504b82f63c10669dc8f
Awaizkazi <Owaizkazi789@gmail.com>
2022-10-02 23:38:36 +0530

    c

---
+++ lib/home_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: Icon(
+          Icons.arrow_back_ios,
+          color: Colors.black,
+        ),
+        actions: [
+          Icon(
+            Icons.shopping_cart,
+            color: Colors.black,
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    'ShopX',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.w900,
+                      fontFamily: 'avenir',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.view_list_rounded),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.grid_view),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
