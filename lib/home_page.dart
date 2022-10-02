7a9dd193edc4738b539e7b951c47d41ac9ff0931
Awaizkazi <Owaizkazi789@gmail.com>
2022-10-02 23:38:40 +0530

    c

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -52,6 +53,7 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
           ),
+          StaggeredGrid.count(crossAxisCount: 2)
         ],
       ),
     );
