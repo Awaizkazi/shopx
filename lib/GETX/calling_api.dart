b14f399ab9969992eff26cfc033498e13cb1e09b
Awaizkazi <Owaizkazi789@gmail.com>
2022-10-05 23:52:45 +0530

    api

---
+++ lib/GETX/calling_api.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CallingAPI extends StatefulWidget {
+  const CallingAPI({super.key});
+
+  @override
+  State<CallingAPI> createState() => _CallingAPIState();
+}
+
+class _CallingAPIState extends State<CallingAPI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
